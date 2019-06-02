e6b3052cc673509a64a713086d1b6a412ef7b783
Brenner Heintz <brenner.heintz@gmail.com>
2019-06-01 22:44:29 -0700

    Add drop_duplicates.

--- databricks/koalas/frame.py
+++ databricks/koalas/frame.py
@@ -2905,6 +2905,36 @@ defaultdict(<class 'list'>, {'col..., 'col...})]
                          index=Metadata(data_columns=data_columns,
                                         index_map=[('summary', None)])).astype('float64')
 
+    def drop_duplicates(self, subset: Optional[Union[str, List[str]]] = None,
+                        keep: Optional[Union[str, bool]] = 'first',
+                        inplace: Optional[bool] = False) -> 'DataFrame':
+        """
+        Return DataFrame with duplicate rows removed, optionally only
+        considering certain columns.
+
+        Parameters
+        ----------
+        subset : column label or sequence of labels, optional
+            Only consider certain columns for identifying duplicates, by
+            default use all of the columns
+        keep : {'first', 'last', False}, default 'first'
+            - ``first`` : Drop duplicates except for the first occurrence.
+            - ``last`` : Drop duplicates except for the last occurrence.
+            - False : Drop all duplicates.
+        inplace : boolean, default False
+            Whether to drop duplicates in place or to return a copy
+
+        Returns
+        -------
+        deduplicated : DataFrame
+        """
+        if keep is not 'first':
+            raise NotImplementedError('Only keep=\'first\' is currently implemented.')
+        sdf = self._sdf.drop_duplicates(subset)
+        if inplace:
+            self._sdf = sdf
+        return DataFrame(sdf, self._metadata.copy())
+
     def _pd_getitem(self, key):
         from databricks.koalas.series import Series
         if key is None:
